903034b587c78e988ec7df91d224a27d993567fb
Domochevsky <ilight507@gmail.com>
2020-04-25 14:59:37 +0500

    s

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,28 @@
 var gulp = require('gulp');
-var rename = require('gulp-rename');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
-var browserSync = require('browser-sync').create();
 
+gulp.task('tpl', function () {
+return gulp.src ('./src/tpl/*.html')
+        .pipe(gulp.dest('./build/'));
+});
 
-function css_style(done){
-	
-	gulp.src('./scss/**/*.scss')
-		.pipe(sourcemaps.init())
-		.pipe(sass({
+gulp.task('css', function () {
+return gulp.src ('./src/css/*.css')
+        .pipe(gulp.dest('./build/css/'));
+});
+
+gulp.task('sass', function () {
+return gulp.src ('./src/sass/style.scss')
+        .pipe(sass({
 			errorLogToConsole: true,
 			//outputStyle: 'compressed'
 		}))
 		.on('error', console.error.bind(console))
-        .pipe(autoprefixer({
-            cascade: false
-        }))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(sourcemaps.write('./'))
-		.pipe( gulp.dest('./css/') )
-		.pipe(browserSync.stream());
-	done();
-}	
-function sync(done) {
-	browserSync.init({
-		server: {
-			baseDir: "./"
-		},
-		port: 3000
-	});
-	done();
-}
-function browserReload(done) {
-	browserSync.reload();
-	done();
-}
-function print(done) {
-	console.log("hi");
-	done();
-}
-function watchSass() {
-	gulp.watch("./scss/**/*", css_style);
-}
-function watchFiles() {
-	gulp.watch("./scss/**/*", css_style);
-	gulp.watch("./**/*.html", browserReload);
-	gulp.watch("./**/*.php", browserReload);
-	gulp.watch("./**/*.js", browserReload);
-}
-//gulp.task(css_style);
-//gulp.task(print);
-gulp.task('default', gulp.parallel(sync, watchFiles));
+        .pipe(gulp.dest('./build/css/'));
 
-gulp.task(sync);
-//exports.printHello = printHello;
+});
+
+gulp.task('default', gulp.parallel('tpl', 'css', 'sass'));
+
+
+
